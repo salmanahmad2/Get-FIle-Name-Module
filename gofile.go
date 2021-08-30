03b56720e750a06b14df642392bebd21e7ef5070
salmanahmad2 <hafizmaliksalman@gmail.com>
2021-08-30 15:32:14 +0500

    first

---
+++ gofile.go
@@ -0,0 +1,14 @@
+package filesModule
+
+import (
+	"fmt"
+	"io/fs"
+)
+
+func files(directory []fs.FileInfo) {
+
+	//file.Name() will get all the name of the files in the folder
+	for _, file := range directory {
+		fmt.Println(file.Name())
+	}
+}
