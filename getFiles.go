73368167085fab17e7b487e8386e1820a5613bbc
Salman Ahmad <88362552+salmanahmad2@users.noreply.github.com>
2021-09-03 13:10:43 +0500

    Update getFiles.go

--- getFiles.go
+++ getFiles.go
@@ -5,7 +5,7 @@ import (
 	"io/fs"
 )
 
-func files(directory []fs.FileInfo) {
+func Files(directory []fs.FileInfo) {
 
 	//file.Name() will get all the name of the files in the folder
 	for _, file := range directory {
